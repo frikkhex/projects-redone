1ec7cb37d199aa19a842172a3633f31b79c3a16b
frikkhex <frikkhex@gmail.com>
2021-09-30 20:17:27 +0200

    added js and output

--- project-1/js/script.js
+++ project-1/js/script.js
@@ -1,1 +1,53 @@
+/* 
+Feedback from Erik Solhaug
+
+# Try shortening (re-factor) the code
+# Utilize forEach method to increase readability of code 
+
+*/
+
 const url = "https://api.coinlore.net/api/tickers/";
+const coinContainer = document.querySelector(".table");
+
+const fetchData = async () => {
+  try {
+    const response = await fetch(url);
+    const result = await response.json();
+    const data = result.data;
+
+    let trend = "";
+
+    data.forEach((el, index) => {
+      const id = el.id;
+      const name = el.name;
+      const shortName = el.symbol;
+      const priceUsd = el.price_usd;
+      const price24h = el.percent_change_24h;
+
+      if (index === 10) throw {};
+
+      if (price24h < 0) {
+        trend = "negative";
+      } else {
+        trend = "positive";
+      }
+
+      coinContainer.innerHTML += `
+        <div class="tbody">
+          <div class="tbody-coin">
+            <p>${name}</p>
+            <p>${shortName}</p>
+          </div>
+          <div class="tbody-info">
+            <p>$${priceUsd}</p>
+            <p class="${trend}">${price24h}%</p>
+          </div>
+        </div>
+      `;
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+fetchData();
